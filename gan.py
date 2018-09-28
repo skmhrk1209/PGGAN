8966a92350788d4447e661efbb0bae7bbedc0cc7
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-29 05:49:08 +0900

    update

--- gan.py
+++ gan.py
@@ -26,77 +26,79 @@ class Model(object):
 
     def __init__(self, model_dir, dataset, generator, discriminator, hyper_param):
 
-        self.model_dir = model_dir
-        self.dataset = dataset
-        self.generator = generator
-        self.discriminator = discriminator
-        self.hyper_param = hyper_param
+        with tf.variable_scope("pggan", reuse=tf.AUTO_REUSE):
 
-        self.batch_size = tf.placeholder(dtype=tf.int32, shape=[])
-        self.training = tf.placeholder(dtype=tf.bool, shape=[])
+            self.model_dir = model_dir
+            self.dataset = dataset
+            self.generator = generator
+            self.discriminator = discriminator
+            self.hyper_param = hyper_param
 
-        self.next_reals = self.dataset.get_next()
-        self.next_latents = tf.random_normal(shape=[self.batch_size, self.hyper_param.latent_size])
+            self.batch_size = tf.placeholder(dtype=tf.int32, shape=[])
+            self.training = tf.placeholder(dtype=tf.bool, shape=[])
 
-        self.reals = tf.placeholder(dtype=tf.float32, shape=self.next_reals.shape)
-        self.latents = tf.placeholder(dtype=tf.float32, shape=[None, self.hyper_param.latent_size])
+            self.next_reals = self.dataset.get_next()
+            self.next_latents = tf.random_normal(shape=[self.batch_size, self.hyper_param.latent_size])
 
-        self.fakes = self.generator(inputs=self.latents, training=self.training, reuse=tf.AUTO_REUSE)
+            self.reals = tf.placeholder(dtype=tf.float32, shape=self.next_reals.shape)
+            self.latents = tf.placeholder(dtype=tf.float32, shape=[None, self.hyper_param.latent_size])
 
-        self.real_logits = self.discriminator(inputs=self.reals, training=self.training, reuse=tf.AUTO_REUSE)
-        self.fake_logits = self.discriminator(inputs=self.fakes, training=self.training, reuse=tf.AUTO_REUSE)
+            self.fakes = self.generator(inputs=self.latents, training=self.training, reuse=tf.AUTO_REUSE)
 
-        self.generator_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=self.fake_logits, labels=tf.ones_like(self.fake_logits)
-        ))
+            self.real_logits = self.discriminator(inputs=self.reals, training=self.training, reuse=tf.AUTO_REUSE)
+            self.fake_logits = self.discriminator(inputs=self.fakes, training=self.training, reuse=tf.AUTO_REUSE)
 
-        self.discriminator_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=self.real_logits, labels=tf.ones_like(self.real_logits)
-        ))
-        self.discriminator_loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=self.fake_logits, labels=tf.zeros_like(self.fake_logits)
-        ))
+            self.generator_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=self.fake_logits, labels=tf.ones_like(self.fake_logits)
+            ))
 
-        interpolate_coefficients = tf.random_uniform(shape=[self.batch_size, 1, 1, 1], dtype=tf.float32)
-        interpolates = self.reals + (self.fakes - self.reals) * interpolate_coefficients
-        interpolate_logits = self.discriminator(inputs=interpolates, training=self.training, reuse=True)
+            self.discriminator_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=self.real_logits, labels=tf.ones_like(self.real_logits)
+            ))
+            self.discriminator_loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+                logits=self.fake_logits, labels=tf.zeros_like(self.fake_logits)
+            ))
 
-        gradients = tf.gradients(ys=interpolate_logits, xs=interpolates)[0]
-        slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1, 2, 3]) + 0.0001)
+            interpolate_coefficients = tf.random_uniform(shape=[self.batch_size, 1, 1, 1], dtype=tf.float32)
+            interpolates = self.reals + (self.fakes - self.reals) * interpolate_coefficients
+            interpolate_logits = self.discriminator(inputs=interpolates, training=self.training, reuse=True)
 
-        self.gradient_penalty = tf.reduce_mean(tf.square(slopes - 1.0))
-        self.discriminator_loss += self.gradient_penalty * self.hyper_param.gradient_coefficient
+            gradients = tf.gradients(ys=interpolate_logits, xs=interpolates)[0]
+            slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1, 2, 3]) + 0.0001)
 
-        self.generator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="generator")
-        self.discriminator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="discriminator")
+            self.gradient_penalty = tf.reduce_mean(tf.square(slopes - 1.0))
+            self.discriminator_loss += self.gradient_penalty * self.hyper_param.gradient_coefficient
 
-        self.generator_global_step = tf.Variable(initial_value=0, trainable=False)
-        self.discriminator_global_step = tf.Variable(initial_value=0, trainable=False)
+            self.generator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="generator")
+            self.discriminator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="discriminator")
 
-        self.generator_optimizer = tf.train.AdamOptimizer(
-            learning_rate=self.hyper_param.learning_rate, beta1=self.hyper_param.beta1, beta2=self.hyper_param.beta2
-        )
-        self.discriminator_optimizer = tf.train.AdamOptimizer(
-            learning_rate=self.hyper_param.learning_rate, beta1=self.hyper_param.beta1, beta2=self.hyper_param.beta2
-        )
+            self.generator_global_step = tf.Variable(initial_value=0, trainable=False)
+            self.discriminator_global_step = tf.Variable(initial_value=0, trainable=False)
 
-        self.update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-
-        with tf.control_dependencies(self.update_ops):
-
-            self.generator_train_op = self.generator_optimizer.minimize(
-                loss=self.generator_loss,
-                var_list=self.generator_variables,
-                global_step=self.generator_global_step
+            self.generator_optimizer = tf.train.AdamOptimizer(
+                learning_rate=self.hyper_param.learning_rate, beta1=self.hyper_param.beta1, beta2=self.hyper_param.beta2
+            )
+            self.discriminator_optimizer = tf.train.AdamOptimizer(
+                learning_rate=self.hyper_param.learning_rate, beta1=self.hyper_param.beta1, beta2=self.hyper_param.beta2
             )
 
-            self.discriminator_train_op = self.discriminator_optimizer.minimize(
-                loss=self.discriminator_loss,
-                var_list=self.discriminator_variables,
-                global_step=self.discriminator_global_step
-            )
+            self.update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
-        self.saver = tf.train.Saver()
+            with tf.control_dependencies(self.update_ops):
+
+                self.generator_train_op = self.generator_optimizer.minimize(
+                    loss=self.generator_loss,
+                    var_list=self.generator_variables,
+                    global_step=self.generator_global_step
+                )
+
+                self.discriminator_train_op = self.discriminator_optimizer.minimize(
+                    loss=self.discriminator_loss,
+                    var_list=self.discriminator_variables,
+                    global_step=self.discriminator_global_step
+                )
+
+            self.saver = tf.train.Saver()
 
     def initialize(self):
 
