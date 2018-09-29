5fe7be462b745c28a635f947489981e8a4aae7b5
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-29 23:34:03 +0900

    update

--- gan.py
+++ gan.py
@@ -287,16 +287,6 @@ class Model(object):
                     summary = session.run(self.summary, feed_dict=feed_dict)
                     writer.add_summary(summary, global_step=generator_global_step)
 
-                    checkpoint = self.saver.save(
-                        sess=session,
-                        save_path=os.path.join(self.name, "model.ckpt"),
-                        global_step=generator_global_step
-                    )
-
-                    stop = time.time()
-                    print("{} saved ({:.2f} sec)".format(checkpoint, stop - start))
-                    start = time.time()
-
                     fakes = session.run(self.fakes, feed_dict=feed_dict)
                     images = np.concatenate([reals, fakes], axis=2)
                     # images = [utils.scale(image, 0, 1, 0, 255) for image in images]
@@ -307,6 +297,18 @@ class Model(object):
                         cv2.imshow("image", image)
                         cv2.waitKey(100)
                         # cv2.imwrite("generated/image_{}_{}.png".format(i, j), image)
+
+                    if i % 1000 == 0:
+
+                        checkpoint = self.saver.save(
+                            sess=session,
+                            save_path=os.path.join(self.name, "model.ckpt"),
+                            global_step=generator_global_step
+                        )
+
+                        stop = time.time()
+                        print("{} saved ({:.2f} sec)".format(checkpoint, stop - start))
+                        start = time.time()
 
         except tf.errors.OutOfRangeError:
 
