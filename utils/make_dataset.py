099c278489e0d6e26b4dc0862c44b43d2be23cad
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-30 07:28:23 +0900

    update

--- utils/make_dataset.py
+++ utils/make_dataset.py
@@ -10,25 +10,18 @@ args = parser.parse_args()
 
 with tf.python_io.TFRecordWriter(args.filename) as writer:
 
-    for label, directory in enumerate(sorted(glob.glob(os.path.join(args.directory, "*")))):
+    for file in glob.glob(os.path.join(args.directory, "*")):
 
-        for file in glob.glob(os.path.join(directory, "*")):
-
-            writer.write(
-                record=tf.train.Example(
-                    features=tf.train.Features(
-                        feature={
-                            "path": tf.train.Feature(
-                                bytes_list=tf.train.BytesList(
-                                    value=[file.encode("utf-8")]
-                                )
-                            ),
-                            "label": tf.train.Feature(
-                                int64_list=tf.train.Int64List(
-                                    value=[label]
-                                )
+        writer.write(
+            record=tf.train.Example(
+                features=tf.train.Features(
+                    feature={
+                        "path": tf.train.Feature(
+                            bytes_list=tf.train.BytesList(
+                                value=[file.encode("utf-8")]
                             )
-                        }
-                    )
-                ).SerializeToString()
-            )
+                        )
+                    }
+                )
+            ).SerializeToString()
+        )
