4264a50d7a27c8c835709ae399c517c3b3cf6e90
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-09-30 00:49:05 +0900

    update

--- gan.py
+++ gan.py
@@ -26,6 +26,8 @@ class Model(object):
 
     def __init__(self, dataset, generator, discriminator, hyper_param, name="gan", reuse=None):
 
+        # if train this model in PGGAN style
+        # set reuse=tf.AUTO_REUSE
         with tf.variable_scope(name, reuse=reuse):
 
             self.name = name
@@ -45,6 +47,11 @@ class Model(object):
                 name="training"
             )
 
+            ### [CAUTION] ###
+            # if assign get_next() to input data tensor,
+            # running any operation that depends on input data tensor
+            # advances input data iterator!
+            # so, use placeholder for reals
             self.next_reals = self.dataset.get_next()
 
             self.next_latents = tf.random_normal(
@@ -82,6 +89,8 @@ class Model(object):
                 reuse=True
             )
 
+            # minimize JS Divergence(GAN)
+            # instead of Wasserstein Distance(WGAN)
             self.generator_loss = tf.reduce_mean(
                 tf.nn.sigmoid_cross_entropy_with_logits(
                     logits=self.fake_logits,
@@ -102,6 +111,9 @@ class Model(object):
                 )
             )
 
+            # add WGAN-GP gradient penalty to discriminator loss
+            # slopes throws NaN (https://github.com/tdeboissiere/DeepLearningImplementations/issues/68)
+            # so add epsilon inside sqrt()
             self.interpolate_coefficients = tf.random_uniform(shape=[self.batch_size, 1, 1, 1], dtype=tf.float32)
             self.interpolates = self.reals + (self.fakes - self.reals) * self.interpolate_coefficients
             self.interpolate_logits = discriminator(inputs=self.interpolates, training=self.training, name="discriminator", reuse=True)
@@ -121,6 +133,7 @@ class Model(object):
                 scope="{}/discriminator".format(self.name)
             )
 
+            # it's ok generator global step and discriminator global step isn't same
             self.generator_global_step = tf.get_variable(
                 name="generator_global_step",
                 shape=[],
@@ -136,6 +149,7 @@ class Model(object):
                 trainable=False
             )
 
+            # tune hyper parameter learning rate, beta1, beta2
             self.generator_optimizer = tf.train.AdamOptimizer(
                 learning_rate=self.hyper_param.learning_rate,
                 beta1=self.hyper_param.beta1,
@@ -147,6 +161,9 @@ class Model(object):
                 beta2=self.hyper_param.beta2
             )
 
+            # to update moving_mean and moving_variance for batch normalization when trainig,
+            # run update operation before run train operation
+            # this update operation is placed in tf.GraphKeys.UPDATE_OPS
             with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
 
                 self.generator_train_op = self.generator_optimizer.minimize(
@@ -161,8 +178,10 @@ class Model(object):
                     global_step=self.discriminator_global_step
                 )
 
+            # save variables already defined
             self.saver = tf.train.Saver()
 
+            # setup summaries of important tensor
             self.summary = tf.summary.merge([
                 tf.summary.image("reals", self.reals),
                 tf.summary.image("fakes", self.fakes),
@@ -171,6 +190,8 @@ class Model(object):
                 tf.summary.scalar("gradient_penalty", self.gradient_penalty),
             ])
 
+    # call this when train model untrained or still training
+    # in this case, model can restore variables from checkpoint.
     def initialize(self):
 
         session = tf.get_default_session()
@@ -186,6 +207,8 @@ class Model(object):
             session.run(tf.variables_initializer(global_variables))
             print("global variables in {} initialized".format(self.name))
 
+    # call this when train model using pre-trained model
+    # in this case, initialize only uninitialized variables
     def reinitialize(self):
 
         session = tf.get_default_session()
@@ -209,6 +232,7 @@ class Model(object):
 
             start = time.time()
 
+            # initialize dataset iterator
             self.dataset.initialize(
                 filenames=filenames,
                 batch_size=batch_size,
@@ -216,6 +240,9 @@ class Model(object):
                 buffer_size=buffer_size
             )
 
+            ### [CAUTION] ###
+            # variables in pre-trained model depends placeholders that doesn't exist in this instance.
+            # so, search those placeholders in graph, and feed values to them.
             latents_placeholder_names = [
                 "{}:0".format(operation.name)
                 for operation in tf.get_default_graph().get_operations()
