6a2fca6926fb91315cbd3750d9552fdb0307bfec
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-01 23:45:16 +0900

    update

--- archs/dcgan.py
+++ archs/dcgan.py
@@ -310,9 +310,6 @@ class Discriminator(object):
                             data_format=self.data_format
                         )
 
-                        print(feature_maps.name)
-                        print(feature_maps.shape)
-
                         return grow(feature_maps, images, index - 1)
 
                     else:
