9212b4d383b2a1f23d7c91091db2f6a9a5ee1ad3
Yosuke Higuchi <wasapon.dev@gmail.com>
2018-10-03 14:56:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import tensorflow as tf
 import argparse
 from models import gan
